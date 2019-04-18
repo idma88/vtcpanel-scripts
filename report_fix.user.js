b3a7a5b37391c8faa6b8be9ce02de3431507854d
Dmitrii Ivanov <idma88@yandex.ru>
2019-04-18 20:15:59 +0300

    Create report_fix.user.js

---
+++ report_fix.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         VTCPanel report page fix
+// @namespace    vtcpanel
+// @version      1.0
+// @description  Исправляет глюк с переносом на странице Мои доклады в Firefox
+// @author       Иванов Дмитрий (aka IDma88)
+// @match        https://vtcpanel.com/*/report
+// @match        https://vtcpanel.com/contracts
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var updateInterval = 1000;
+
+    var reportScaner = setInterval(function() {
+        var spansObj = document.querySelectorAll('.span-reports-org div div span:nth-child(2)[style*="display: inline-table"]');
+        for (var i = 0; i < spansObj.length; i++)
+        {
+            spansObj[i].style.display = "inline";
+        }
+
+        spansObj = document.querySelectorAll('.block_work_kontrakt div div span[style*="display: inline-table"]');
+        for (var i = 0; i < spansObj.length; i++)
+        {
+            spansObj[i].style.display = "inline";
+        }
+
+    }, updateInterval);
+})();
