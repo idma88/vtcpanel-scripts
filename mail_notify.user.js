68518a518e4a3aa9ff40347f75caa00d046532aa
Dmitrii Ivanov <idma88@yandex.ru>
2019-04-18 20:14:23 +0300

    Update mail_notify.user.js

--- mail_notify.user.js
+++ mail_notify.user.js
@@ -1,20 +1,28 @@
 // ==UserScript==
-// @name         Mail icon highlight
+// @name         VTCPanel mail icon highlight
 // @namespace    vtcpanel
-// @version      1.1
+// @version      1.2
 // @description  "Подсвечивает" иконку почты, если есть новые сообщения
-// @author       Ivanov Dmitrii aka IDma88
+// @author       Иванов Дмитрий (aka IDma88)
 // @match        https://vtcpanel.com/*
 // @grant        none
 // ==/UserScript==
 
-var counterEl = document.getElementsByClassName('blo_pochta')[0];
-var count = parseInt(counterEl.textContent);
 
-if (count) {
-    var mailIcon = counterEl.parentNode.getElementsByTagName('i')[0];
-    setInterval(function() {
-        mailIcon.classList.toggle("fa-envelope-o");
-        mailIcon.classList.toggle("fa-envelope");
-    }, 1000);
-}
+(function() {
+    'use strict';
+
+    var flashInterval = 1000;
+
+    var counterEl = document.getElementsByClassName('blo_pochta')[0];
+    var count = parseInt(counterEl.textContent);
+
+    if (count) {
+        var mailIcon = counterEl.parentNode.getElementsByTagName('i')[0];
+        setInterval(function() {
+            mailIcon.classList.toggle("fa-envelope-o");
+            mailIcon.classList.toggle("fa-envelope");
+        }, flashInterval);
+    }
+
+})();
